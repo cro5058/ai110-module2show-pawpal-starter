60c62fefaac4c76a3dbe59599f172884ce3616a0
Ryan O'Malley <cro5058@g.rit.edu>
2026-07-11 18:20:52 -0400

    Added imports and main guard to main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pawpal_system import (
+    Priority, 
+    CompletionStatus, 
+    Pet, 
+    Task, 
+    Owner, 
+    Schedule, 
+    Scheduler
+)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
